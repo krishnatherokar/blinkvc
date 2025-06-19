3c1e07864be7dcdb78c189697a84c2a31b179218
Krishna Therokar <krishnatherokar@gmail.com>
2025-06-19 12:32:06 +0530

    feat: implement Clerk webhook handling for user events

---
+++ frontend/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,36 @@
+import { connectDB } from "@/lib/connectDB";
+import { User } from "@/models/User";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+    const { id } = evt.data;
+    const username = "username" in evt.data ? evt.data.username : null;
+
+    await connectDB();
+
+    switch (evt.type) {
+      case "user.created":
+        await User.create({
+          clerkId: id,
+          username,
+        });
+        break;
+      case "user.updated":
+        await User.updateOne({ clerkId: id }, { username });
+        break;
+      case "user.deleted":
+        await User.deleteOne({ clerkId: id });
+        break;
+      default:
+        console.log("Unhandled event:", evt.type);
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
