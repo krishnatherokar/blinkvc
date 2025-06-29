dbe36daa36cbca4ee07aebc1d1e6c81023079150
Krishna Therokar <krishnatherokar@gmail.com>
2025-06-29 17:00:49 +0530

    fix: use clerk component to redirect to sign-up page instead of router

--- frontend/app/(authRequired)/layout.tsx
+++ frontend/app/(authRequired)/layout.tsx
@@ -1,24 +1,18 @@
 "use client";
-import Loading from "@/components/Loading";
-import { useAuth } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { RedirectToSignUp, SignedIn, SignedOut } from "@clerk/nextjs";
 
 export default function ProtectedPage({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isSignedIn, isLoaded } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isSignedIn === false)
-      router.replace(`${process.env.NEXT_PUBLIC_CLERK_URL}/sign-up`);
-  }, [isSignedIn]);
-
-  if (!isLoaded) return <Loading />;
-
-  if (isSignedIn) return children;
+  return (
+    <>
+      <SignedIn>{children}</SignedIn>
+      <SignedOut>
+        <RedirectToSignUp />
+      </SignedOut>
+    </>
+  );
   return null;
 }
