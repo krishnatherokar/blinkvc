d34e22cde4407117be5f9e4d63ea23d236940c88
Krishna Therokar <krishnatherokar@gmail.com>
2025-07-02 13:44:48 +0530

    feat: add framer-motion in VideoScreen

--- frontend/components/VideoScreen.tsx
+++ frontend/components/VideoScreen.tsx
@@ -5,6 +5,7 @@ import { AiOutlineAudio, AiOutlineAudioMuted } from "react-icons/ai";
 import { LuDot, LuVideo, LuVideoOff } from "react-icons/lu";
 import { MdOutlineCallEnd, MdOutlineSend } from "react-icons/md";
 import { RxReload, RxSpeakerLoud, RxSpeakerOff } from "react-icons/rx";
+import { AnimatePresence, motion } from "framer-motion";
 
 export type chatType = { sender: string; text: string };
 
@@ -99,7 +100,7 @@ const VideoScreen = ({
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [chat]);
+  }, [chat, isChatVisible]);
 
   useEffect(() => {
     if (data == "Peer disconnected") setIsChatVisible(false);
@@ -120,148 +121,167 @@ const VideoScreen = ({
       {/* Top section */}
 
       <section
-        className={`${
-          data == "connected" ? "bg-none" : "bg-neutral-100 dark:bg-neutral-900"
-        }  fixed z-8 top-0 left-0 w-screen flex
-        sm:bg-transparent`}
+        className="fixed z-8 top-0 left-0 w-screen flex
+        sm:bg-transparent"
       >
-        <video
-          onClick={() => swapVideo()}
-          ref={localvideoRef}
-          autoPlay
-          playsInline
-          className={`${
-            data == "connected" ? "" : "hidden"
-          } max-w-50 max-h-50 object-contain rounded-bl-2xl transform scale-x-[-1]
-          sm:max-w-xs`}
-        />
+        <motion.div
+          layout
+          className={data == "connected" ? "h-fit w-fit" : "fixed inset-0 z-7"}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+        >
+          <video
+            onClick={() => swapVideo()}
+            ref={localvideoRef}
+            autoPlay
+            playsInline
+            className={`${
+              data == "connected"
+                ? "rounded-bl-2xl max-w-50 max-h-50 object-contain sm:max-w-xs"
+                : "h-screen w-screen object-cover"
+            } transform scale-x-[-1]`}
+          />
+        </motion.div>
 
         {/* Dummy transparent section */}
         <section
           className={`${data == "connected" ? "flex-0 md:flex-1" : "flex-1"}`}
         ></section>
 
-        <section
-          className="flex-1
-          md:max-w-xs md:min-w-2xs"
-        >
-          <section
-            className="bg-neutral-100 flex justify-evenly w-full rounded-bl-2xl py-4 text-2xl
-            dark:bg-neutral-900"
-          >
-            <button onClick={toggleVideo}>
-              {isVideoOn ? <LuVideo /> : <LuVideoOff />}
-            </button>
-            <button onClick={toggleAudio}>
-              {isAudioOn ? <AiOutlineAudio /> : <AiOutlineAudioMuted />}
-            </button>
-            <button onClick={toggleMute}>
-              {isMuted ? <RxSpeakerOff /> : <RxSpeakerLoud />}
-            </button>
-          </section>
-        </section>
+        {/* Control buttons section */}
+        <AnimatePresence>
+          {data == "connected" && (
+            <motion.div
+              initial={{ y: -50 }}
+              animate={{ y: 0 }}
+              exit={{ y: -50 }}
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+              className="flex-1 z-8
+              md:max-w-xs md:min-w-2xs"
+            >
+              <section
+                className="bg-neutral-100 flex justify-evenly w-full rounded-bl-2xl py-4 text-2xl
+                dark:bg-neutral-900"
+              >
+                <button onClick={toggleVideo}>
+                  {isVideoOn ? <LuVideo /> : <LuVideoOff />}
+                </button>
+                <button onClick={toggleAudio}>
+                  {isAudioOn ? <AiOutlineAudio /> : <AiOutlineAudioMuted />}
+                </button>
+                <button onClick={toggleMute}>
+                  {isMuted ? <RxSpeakerOff /> : <RxSpeakerLoud />}
+                </button>
+              </section>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </section>
 
       {/* Bottom section */}
-
-      <section
-        className="fixed z-8 bottom-0 right-0 w-screen text-lg bg-neutral-100
-        sm:w-sm rounded-t-2xl sm:rounded-tr-none
-        dark:bg-neutral-900"
-      >
-        {data == "connected" ? (
-          <div
-            className="p-4 text-center"
-            onClick={() => setIsChatVisible(true)}
-          >
-            {chat ? (
-              <>
-                {chat[chat.length - 1].text}
-                <LuDot className="h-6 w-6 inline" />
-              </>
-            ) : null}
-            <span className="text-blue-600">Tap to Chat</span>
-          </div>
-        ) : (
-          <div className="p-4 text-center">{data}</div>
-        )}
-      </section>
-
-      {/* Chat window */}
-
-      <section
-        className={`${
-          isChatVisible ? "" : "hidden"
-        } fixed z-9 bottom-0 right-0 w-screen rounded-t-2xl bg-neutral-100
-        sm:w-sm sm:rounded-tr-none
-        dark:bg-neutral-900`}
-      >
-        <div
-          className="h-40 overflow-y-scroll rounded-xl mx-2 mt-2 py-1 bg-white
-          dark:bg-black"
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={isChatVisible ? 1 : 0}
+          initial={{ y: 200 }}
+          animate={{ y: 0 }}
+          exit={{ y: 200 }}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+          className="fixed z-9 bottom-0 right-0 w-screen text-lg bg-neutral-100 rounded-t-2xl
+          sm:w-sm sm:rounded-tr-none
+          dark:bg-neutral-900"
         >
-          {chat ? (
+          {isChatVisible ? (
             <>
-              {chat.map((message: chatType, i: number) => (
-                <div
-                  key={i}
-                  className={`${
-                    message.sender == "You"
-                      ? "ml-auto rounded-br-none bg-neutral-100 dark:bg-neutral-900"
-                      : "mr-auto rounded-bl-none text-white bg-blue-600"
-                  } w-fit max-w-4/5 px-3 py-2 m-2 rounded-xl`}
-                >
-                  {message.text}
-                </div>
-              ))}
-              <div ref={bottomRef} />
+              {/* Chat window */}
+              <div
+                className="h-40 overflow-y-scroll rounded-xl mx-2 mt-2 py-1 bg-white
+                dark:bg-black"
+              >
+                {chat ? (
+                  <>
+                    {chat.map((message: chatType, i: number) => (
+                      <div
+                        key={i}
+                        className={`${
+                          message.sender == "You"
+                            ? "ml-auto rounded-br-none bg-neutral-100 dark:bg-neutral-900"
+                            : "mr-auto rounded-bl-none text-white bg-blue-600"
+                        } w-fit max-w-4/5 px-3 py-2 m-2 rounded-xl`}
+                      >
+                        {message.text}
+                      </div>
+                    ))}
+                    <div ref={bottomRef} />
+                  </>
+                ) : (
+                  <div
+                    className="flex flex-col h-full text-center justify-center text-neutral-300 p-4
+              dark:text-neutral-700"
+                  >
+                    No messages
+                  </div>
+                )}
+              </div>
+
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  sendMessage();
+                }}
+                className="w-full flex"
+              >
+                <input
+                  ref={chatInputRef}
+                  type="text"
+                  maxLength={60}
+                  placeholder="Type here..."
+                  className="flex-1 rounded-sm p-2 m-2 bg-white
+                  dark:bg-black"
+                  autoComplete="off"
+                  required
+                  autoFocus
+                />
+                <button type="submit">
+                  <MdOutlineSend className="h-7 w-7 m-2" />
+                </button>
+              </form>
             </>
           ) : (
-            <div
-              className="flex flex-col h-full text-center justify-center text-neutral-300 p-4
-              dark:text-neutral-700"
-            >
-              No messages
-            </div>
+            <>
+              {/* Bottom info section */}
+              {data == "connected" ? (
+                <div
+                  className="p-4 text-center"
+                  onClick={() => setIsChatVisible(true)}
+                >
+                  {chat ? (
+                    <>
+                      {chat[chat.length - 1].text}
+                      <LuDot className="h-6 w-6 inline" />
+                    </>
+                  ) : null}
+                  <span className="text-blue-600">Tap to Chat</span>
+                </div>
+              ) : (
+                <div className="p-4 text-center">{data}</div>
+              )}
+            </>
           )}
-        </div>
-
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            sendMessage();
-          }}
-          className="w-full flex"
-        >
-          <input
-            ref={chatInputRef}
-            type="text"
-            maxLength={60}
-            placeholder="Type here..."
-            className="flex-1 rounded-sm p-2 m-2 bg-white
-            dark:bg-black"
-            autoComplete="off"
-            required
-          />
-          <button type="submit">
-            <MdOutlineSend className="h-7 w-7 m-2" />
-          </button>
-        </form>
-      </section>
+        </motion.div>
+      </AnimatePresence>
 
       {/* Refresh and end call buttons */}
 
       {setCount ? (
         <button
           onClick={reload}
-          className="fixed z-7 bottom-20 right-2 p-3 rounded-lg text-white bg-blue-700"
+          className="fixed z-8 bottom-20 right-2 p-4 rounded-2xl text-white bg-blue-600"
         >
           <RxReload className="h-8 w-8" />
         </button>
       ) : (
         <button
           onClick={() => router.back()}
-          className="fixed z-7 bottom-20 right-2 p-3 rounded-lg text-white bg-red-500"
+          className="fixed z-8 bottom-20 right-2 p-4 rounded-2xl text-white bg-red-500"
         >
           <MdOutlineCallEnd className="h-8 w-8" />
         </button>
